12896cd5102dcf6ec4fc2232af97a6847e65afc6
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-08-14 20:08:27 +0200

    Clouds.js modified removed image

--- src/components/Clouds.js
+++ src/components/Clouds.js
@@ -5,7 +5,6 @@ export default function Clouds(){
         <img src="/clouds/cloud1.png" alt="Cloud 1" style={{'--i': 1.5 }}/>
         <img src="/clouds/cloud2.png" alt="Cloud 2" style={{'--i': 2 }}/>
         <img src="/clouds/cloud3.png" alt="Cloud 3" style={{'--i': 3 }}/>
-        <img src="/hello_there.png" id="Hello" alt="Cloud 3"/>
         <img src="/clouds/cloud4.png" alt="Cloud 4" style={{'--i': 4 }}/>
         <img src="/clouds/cloud5.png" alt="Cloud 5" style={{'--i': 5 }}/>
     </div>
