2b0dfb4e61b7ee4742c6c180ffd89c91dfee218b
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-08-14 20:08:07 +0200

    Nav.js modified Changed names

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,26 @@
 import "../app/globals.css"
 
-export default function Nav(){
-    return  <div className="bg-black-100 font-sans">
-        <nav className="bg-black-100 fixed w-full top-0 ">
-            <div className="container mx-auto py-4">
-                <div className="flex justify-center space-x-10">
-                    <a href="#engagement" className="nav-link transition duration-300">
-                        Stack
-                    </a>
-                    <a href="#projekte" className="nav-link transition duration-300">
-                        Projects
-                    </a>
-                    <a href="#hobbies" className="nav-link transition duration-300">
-                        Hobbys
-                    </a>
-                    <a href="#contact" className="nav-link transition duration-300">
-                        Contact
-                    </a>
+export default function Nav() {
+    return (
+        <div className="bg-black-100 font-sans">
+            <nav className="w-full top-0 fixed">
+                <div className="container mx-auto py-4">
+                    <div className="flex justify-center space-x-10">
+                        <a href="#stack" className="nav-link transition duration-300">
+                            Stack
+                        </a>
+                        <a href="#projekte" className="nav-link transition duration-300">
+                            Projects
+                        </a>
+                        <a href="#hobbies" className="nav-link transition duration-300">
+                            Hobbys
+                        </a>
+                        <a href="#contact" className="nav-link transition duration-300">
+                            Contact
+                        </a>
+                    </div>
                 </div>
-            </div>
-
-        </nav>
-
-
-
-    </div>
+            </nav>
+        </div>
+    );
 }
