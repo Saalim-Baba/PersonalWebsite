45df3daa43aa6c316a31fc875483e3f4653870a4
milaasHD <saalim.baba@outlook.com>
2024-08-19 00:07:12 +0200

    Box.js Box implemented and its pure css

--- src/app/page.js
+++ src/app/page.js
@@ -9,17 +9,63 @@ import Loader from "../components/Loader"
 import Image from 'next/image'
 import {useEffect} from "react";
 import Box from "../components/Box"
+import Grass from "../components/Grass"
+import Blob from "../components/Blob"
 
 const HomePage = () => {
     useEffect(() => {
+        const circles = document.querySelectorAll(".circle");
+        const box_lid = document.getElementById("box_lid");
+
         window.onbeforeunload = function () {
             window.scrollTo(0, 0);
-        }
+        };
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting){
+                    entry.target.classList.add("show");
+                    if (entry.target.classList.contains("box_reveal")){
+                        if (!box_lid.classList.contains("lid")) {
+                            box_lid.classList.add("lid");
+                            circles.forEach((circle, index) => {
+                                setTimeout(() => {
+                                    circle.classList.toggle("fade-out");
+                                }, index * 200);
+                            });
+                        }
+                    }
+
+                } else {
+                    entry.target.classList.remove("show");
+                    if (entry.target.classList.contains("box_reveal")){
+                        if (box_lid.classList.contains("lid")){
+                            box_lid.classList.remove("lid");
+                            circles.forEach((circle, index) => {
+                                setTimeout(() => {
+                                    circle.classList.toggle("fade-out");
+                                }, index * 200);
+                            });
+                        }
+
+                    }
+                }
+            });
+        });
+
+        const hiddenElements = document.querySelectorAll(".hidden_raw");
+        hiddenElements.forEach((el) => observer.observe(el));
+
+        return () => {
+            observer.disconnect();
+            window.onbeforeunload = null;
+        };
     }, []);
+
 
     return (
         <>
-            <div id="image-top" className={"flex flex-col justify-center"}>
+            <div id="image-top" className="flex flex-col justify-center mb-32 ">
                 <Nav/>
                 <Image
                     src="/sun.png"
@@ -31,8 +77,23 @@ const HomePage = () => {
                 <Name/>
                 <Clouds/>
             </div>
-            <div id="stack">
+            <Blob/>
+            <div id="stack" className="mt-2">
+                <h1 className="stack-cool z-0 ml-20 hidden_raw">
+                    STACK
+                </h1>
                 <Box/>
+            </div>
+            <div id="projects" className="mt-72 hidden_raw">
+                <h1 className="stack-cool z-0 ml-20">
+                    Projects
+                </h1>
+                <div className="relative flex flex-col justify-end items-center bottom-0 h-[70vh]">
+
+                </div>
+            </div>
+            <div>
+                <Grass/>
             </div>
 
         </>
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,23 +1,76 @@
+import React, { useEffect } from "react";
 
 export default function Box() {
+
     return (
         <>
-            <div className="flex flex-col justify-end items-center bottom-0 border border-black h-[70vh]">
-                <div className="box">
-                    <div className="box__face box__face--front"></div>
-                    <div className="box__face box__face--top"></div>
-                    <div className="box__face box__face--left">
-                        <br/>
-                        <h1 className="ml-4">My Stack</h1>
+
+
+            <div className="relative flex flex-col justify-end items-center bottom-0 h-[70vh] box-container hidden_raw box_reveal">
+                <img
+                    className="circle"
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+                    alt="python"
+                />
+                <img
+                    className="circle"
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                    alt="javascript"
+                />
+                <img
+                    className="circle"
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mariadb/mariadb-original-wordmark.svg"
+                    alt="mariadb"
+                />
+                <img
+                    className="circle"
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg"
+                    alt="tailwindcss"
+                />
+                <img
+                    className="circle"
+                     src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
+                     alt="cpp"
+                />
+                <img
+                    className="circle"
+                    src=""
+                    alt=""
+                />
+                <img
+                    className="circle"
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original-wordmark.svg"
+                    alt="nextjs"
+                />
+                <img
+                    className="circle"
+                    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+                    alt="nodejs"
+                />
+                <button id="box-id">
+                    <div className="box absolute ">
+                        <div className="box__face box__face--front"></div>
+                        <div id="box_lid" className="box__face box__face--top"></div>
+                        <div className="box__face box__face--left flex flex-col justify-center items-center">
+                            <h1 className="mb-2 text-[1vw] text-black">My Stack</h1>
+                        </div>
+                        <div className="box_shadow">
+                        </div>
+                        <div className="relative box__face box__face--right">
+                            <img
+                                src="/web-icon.png"
+                                className="hidden border border-black h-[3vw] w-[3vw] absolute bottom-0 right-0 mr-[6vw] [transform:rotateY(10deg)]"
+                                alt="web icon"
+                            />
+                            <img
+                                src="/info-icon.png"
+                                className="hidden border border-black h-[3vw] w-[3vw]  absolute bottom-0  mr-[-2vw] [transform:rotateY(10deg)]"
+                                alt="info icon"
+                            />
+                        </div>
+                        <div className="box__face box__face--back"></div>
                     </div>
-                    <div className="relative box__face box__face--right">
-                        <img src="/web-icon.png" className="hidden border border-black h-6 w-6  absolute bottom-0 right-0 m-2 [transform:rotateY(10deg)]">
-                        </img>
-                        <img src="/info-icon.png" className="hidden border border-black h-6 w-6  absolute bottom-0 right-7 m-2 [transform:rotateY(10deg)]">
-                        </img>
-                    </div>
-                    <div className="box__face box__face--back"></div>
-                </div>
+                </button>
             </div>
         </>
     );
