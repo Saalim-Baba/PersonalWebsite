d9d975ee9cb21c94eecdc41c2ad038046a66134d
milaasHD <saalim.baba@outlook.com>
2024-08-19 00:09:20 +0200

    its a prototype, has lots to work on fo

---
+++ src/components/Grass.js
@@ -0,0 +1,19 @@
+import "../app/globals.css"
+
+export default function Grass(){
+    return <>
+        <div className="footer">
+            <div className="grass"></div>
+            <div className="soil">
+                <div className="rocks"></div>
+                <div className="links">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                </div>
+            </div>
+        </div>
+
+
+    </>
+}
