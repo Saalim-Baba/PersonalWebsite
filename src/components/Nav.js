d8da16b679e2d560c022fd9f93cd8826ba8ad974
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-08-27 23:45:36 +0200

    changed some things like opacity

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import "../app/globals.css"
 export default function Nav() {
     return (
         <div className="font-sans text-center align-center">
-            <nav className="w-[40vw] bg-gray-200 rounded-md bg-opacity-80 fixed top-0 left-1/2 transform -translate-x-1/2 z-[1000]">
+            <nav className="w-[40vw] bg-gray-200 rounded-md bg-opacity-50 fixed top-0 left-1/2 transform -translate-x-1/2 z-[1000]">
                 <div className="container py-4">
                     <div className="flex justify-center space-x-10">
                         <a href="#stack" className="nav-link transition duration-300">
