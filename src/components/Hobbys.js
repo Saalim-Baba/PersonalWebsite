4a216945622ee79dcdecfe451564e741e60c3963
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-08-27 21:41:18 +0200

    Made some cards that flip, still have to design them tho

---
+++ src/components/Hobbys.js
@@ -0,0 +1,40 @@
+import "../app/globals.css";
+
+
+export default function Hobbys(){
+
+    const hobbys = [
+        { name: "Wandern", description: "Genieße die Natur beim Wandern.", margin: 12 },
+        { name: "Krafttraining", description: "Stärke deinen Körper im Gym.", margin: 32 },
+        { name: "Technik", description: "Löse Probleme durch Code.", margin: 52 },
+        { name: "Skisport", description: "Erlebe den Winter auf Skiern.", margin: 72 },
+    ];
+
+
+
+    return (
+        <>
+        <div className="w-full ">
+            {hobbys.map((hobby, index) => (
+                <div
+                    key={index}
+                    className="mt-20"
+                >
+                    <div style={{ marginLeft: `${hobby.margin}vw` }}>
+                        <div className="thd_card">
+                            <div className="face front ">
+                                <p>{hobby.name}</p>
+                            </div>
+                            <div className="face back">
+                                <p>{hobby.description}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            ))}
+        </div>
+
+        </>
+    );
+}
