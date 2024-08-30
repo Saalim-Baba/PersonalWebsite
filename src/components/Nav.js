c0bd697ebf521dfb56fa5e59f23a77788b65aefe
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-08-30 11:22:50 +0200

    made navbar smaller

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
 import "../app/globals.css"
+import Image from "next/image";
 
 export default function Nav() {
     return (
         <div className="font-serif text-center align-center">
-            <nav className="w-[40vw] bg-gray-200 rounded-md bg-opacity-50 fixed top-5 left-1/2 transform -translate-x-1/2 z-[1000]">
+            <nav className="w-[30vw] bg-gray-200 rounded-md bg-opacity-50 fixed top-5 left-1/2 transform -translate-x-1/2 z-[1000]">
                 <div className="container py-4">
                     <div className="flex justify-center space-x-10">
                         <a href="#stack" className="nav-link transition duration-300">
